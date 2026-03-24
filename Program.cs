20cec2cebe3116304d382376b38f1a6ec8d4b25a
tokicanja <anjatokic79@gmail.com>
2026-03-24 19:27:28 +0100

    Remove duplicate greeting and add new message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, anja! :) ");
 Console.WriteLine("Hello, anja! :) ");
+Console.WriteLine("Hello, anja je najbolja! :) ");
